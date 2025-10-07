9ea6065d74e7e7a03cd9add77a1b8589116212b0
Wiley Welch <wileyjwelch1@gmail.com>
2025-10-06 18:46:25 -0600

    Jobs is now protected

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import Navbar from "@/components/ui/navBar";
 import JobTable from "@/components/jobTableComp/jobTable";
+import { getAuthUser } from "@/lib/auth/utils";
+import { redirect } from "next/navigation";
 
 export default async function Jobs() {
   // Otherwise, render the landing page
+  const user = await getAuthUser();
+  if (!user) {
+    redirect("/login");
+  }
+
   return (
     <div>
       <Navbar />
